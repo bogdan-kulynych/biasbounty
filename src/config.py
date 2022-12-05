b0eefbd753bceb09a345076eb27da189be059b33
Bogdan Kulynych <hello@bogdankulynych.me>
2022-12-05 10:01:27 +0100

    Change the name

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 rel_train_data_path = "data/train"
 rel_test_data_path = "data/test"
 rel_submission_path = "out/submission.json"
-submission_title = "atleastwetried"
+submission_title = "wellwetried"
